92223e6c92ff414640be755b01ac6fd846b55c53
ahmedgalalmohamed2016 <ahmedgalalmohamed2016@gmail.com>
2020-01-15 13:35:34 +0200

    first push

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./configs/main');
+const path = require('path');
+
+const merchant = require('./routes/merchant.route');
+const userRoutes = require('./routes/user.route');
+const transactionRoutes = require('./routes/transaction.route');
+const cardRoutes = require('./routes/card.route');
+const walletRoutes = require('./routes/wallet.route');
+
+const app = express();
+// Set up mongoose connection
+const mongoose = require('mongoose');
+// let dev_db_url = 'mongodb://admin:2wGnLj9ayKfeZLCQaVgy5WvBW4nuQemsy977BvdbJykmjq4c@ds045679.mlab.com:45679/hiddenholidaydb';
+let dev_db_url = 'mongodb://admin:Hidden123@ds119129.mlab.com:19129/hiddenholidaystable';
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const options = {
+    keepAlive: 1,
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+};
+mongoose.connect(mongoDB, options);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/merchants', merchant);
+app.use('/user', userRoutes);
+app.use('/api/transactions', transactionRoutes);
+app.use('/api/cards', cardRoutes);
+app.use('/api/wallet', walletRoutes);
+
+let port = 3000;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
